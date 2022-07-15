5f7b5f31d43e26078aab42b9fde5dd00d1dcbcde
Amir <amirhossein7731377@gmail.com>
2022-07-15 22:04:17 +0430

    +

--- utils/config.js
+++ utils/config.js
@@ -6,5 +6,5 @@ export const colors = {
 
 export const api = {
   main: "https://api.gharbonlineconnect.ir/",
-  socket: "https://socket.inpointconnect.ir/",
+  socket: "https://socket.gharbonlineconnect.ir/",
 };
